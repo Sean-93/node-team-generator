40e0a842936bf4659bec03bf1b004a0756256121
Sean Rooney <seandanielrooney@gmail.com>
2020-03-03 21:46:46 -0700

    added prototypes

--- app.js
+++ app.js
@@ -4,25 +4,48 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-
+​const render = require("./lib/htmlRenderer");
 console.log(new Manager("name", 1, "fakeemail1@fake.com", 214));
 console.log(new Engineer("name", 1, "fakeemail1@fake.com", 214));
 console.log(new Manager("name", 1, "fakeemail1@fake.com", 214));
-​
-​
-// Write code to use inquirer to gather information about the development team members,
+​// Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
+function start(){
+    function createManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your manager's name?"
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is your manager's ID?"
+            },
+            {
+                type: "input",
+                name: "managerOffice",
+                message: "What is your manager's office number?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your manager's email address?"
+            }
+        ]).then(function(data){
+            console.log(data);
+        })
+        
+    }
+    
+}
+​// After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
+​// After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
